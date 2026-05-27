b35b7d036bd662b81a81218affe53f8cf046c60c
yash-deshpande24 <deshpandeyash945@gmail.com>
2026-05-27 10:24:35 +0530

    Initial project structure

--- cloudtask-manager/terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "ec2_public_ip"       { value = aws_instance.app.public_ip }
-output "ec2_public_dns"      { value = aws_instance.app.public_dns }
-output "rds_endpoint"        { value = aws_db_instance.postgres.endpoint }
-output "ecr_backend_url"     { value = aws_ecr_repository.backend.repository_url }
-output "ecr_frontend_url"    { value = aws_ecr_repository.frontend.repository_url }
-output "cloudwatch_dashboard"{ value = "https://${var.aws_region}.console.aws.amazon.com/cloudwatch/home#dashboards:name=${var.project_name}-dashboard" }
--- cloudtask-manager/terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region"     { default = "us-east-1" }
-variable "project_name"   { default = "cloudtask" }
-variable "key_pair_name"  { description = "Your EC2 key pair name" }
-variable "alert_email"    { description = "Email for CloudWatch alarms" }
-variable "db_password"    { description = "RDS PostgreSQL password" sensitive = true }
